40009571718df025d033751747ee0fa0e9623b00
P2-openclassroom <jonh_06@live.fr>
2022-03-12 09:47:07 +0100

    change file app

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,8 @@ const app = express();
 module.exports = app;
 
 
-//*********Creation de Middleware********
+//--------- Middleware (Creation)-----------
 //next(); // pour renvoyer vers le prochain middleware
-
-
 //permet a l'application d'accedera l'api
 app.use((req, res, next) => {
     // * signifie all tous le monde  a acces au serveur origin
@@ -20,33 +18,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //autorise certaine methode requete
     next();
 });
+//-------------
 
 
 // reponse par default et donc retourn bien notre application on recoit l'objet request response et next pour passez a la suivante
 
-//***route get pour recuperer tous le objet du site*******
-app.use('/api/stuff', (req, res, next) => { //url viser (route a contacter) par l'application route http://localhost:3000
-    
-    //création des objet-----------
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',    //clée valeur
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900, //evite les chiffre a virgule , centimes pour eviter les erreurs d'arithmétique (calcul)
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    //------------------
+// -------------ROUTE ------------// get pour recuperer tous le objet du site
+app.get("/", (req ,res ) => res.send("Hello World"));
+app.get('/api/sauces', (req, res, next)); //url viser (route a contacter) par l'application route http://localhost:3000
 
-    res.status(200).json(stuff); // retourne le status ex 200 pour ok pour la methode http , revoi la reponse json (body) stuff
-});
+
