4f30d6874e0570d6e34379713f8e6fc41c23a37a
P2-openclassroom <jonh_06@live.fr>
2022-03-12 15:55:25 +0100

    add contenue route login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,4 @@
-/// Controller utilisateur------------- (contenue route utilisateur)
+/// Controller utilisateur------------- (contenue route utilisateur  POST)
 
 //enregister user dans middleware ,importer dans le fichier
 const User = require('../models/User');
@@ -15,11 +15,11 @@ exports.signup = (req, res, next) => { //function pour crypter un mot de pass , 
         email: req.body.email, // email passez l'addresse passsez dans le corp de la requete
         password :hash // enregistrer le mdp crypter (hash) pour ne pas stocker un mdp en blanc
         });
-    user.save() //enregistre dans la BDD    
-    .then(() => res.status(201).json({message: 'utilisateur créé !'})) //creation de ressource
-    .catch(error => res.status(400).json({ error })); //impossible de se connecter au serveur
-})
-.catch(error => res.status(500).json({ error })); // 500 erreur serveur renvoi erreur dans objet
+        user.save() //enregistre dans la BDD    
+        .then(() => res.status(201).json({message: 'utilisateur créé !'})) //creation de ressource
+        .catch(error => res.status(400).json({ error })); //impossible de se connecter au serveur
+    })
+    .catch(error => res.status(500).json({ error })); // 500 erreur serveur renvoi erreur dans objet
 };  
 
 
@@ -28,7 +28,27 @@ exports.signup = (req, res, next) => { //function pour crypter un mot de pass , 
 //function login pour connecter de nouveaux utilisateur existant
 
 exports.login = (req, res, next) => {
-
+    //trouver un seul utilisateur de la BD grace a (unique)    
+    User.findOne({ email: req.body.email }) //on veut que email correspond a la req
+    .then(user => {
+    //si user (!user) n'est pas trouver  on renvoi le return message d'error
+    if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    } //sinon l'user a etait trouver et on compare le mdp crypte de l'utilisateur qui se connect
+    bcrypt.compare(req.body.password, user.password) // function pour comparer les mdp hash (crypter) envoyer par la req (objet body envoyer)
+    // et le hash deja enregistrer
+    .then(valid => {  //recoi un boolean (true false comparaison valid ou non)
+        if (!valid) { //false mauvais mdp
+            return res.status(401).json({ error: 'Mot de passe incorrect !' }); 
+        } // sinon true on continue
+        res.status(200).json({
+            userId: user._id, //id de l'utilisateur das la base
+            token: 'TOKEN' //token crypter pour permettre la connection de l'utilisateur
+        }); //connection valider
+    }) 
+    .catch(error => res.status(500).json({ error }));   
+    })
+    .catch(error => res.status(500).json({ error })); //erreur serveur (problem conenction mongo db)
 };
 
 //----------------
