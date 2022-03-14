e0cd44eae24eebffb57770cd66448ecd26e2c07e
P2-openclassroom <jonh_06@live.fr>
2022-03-14 11:23:45 +0100

    correction user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,18 @@
-/// Controller utilisateur------------- (contenue route utilisateur)
+// faciliser les tache ecriture lecture de mongo DB
 
-//enregister user dans middleware ,importer dans le fichier
-const User = require('../models/User');
-const bcrypt = require ('bcrypt') //importer package de cryptage
+// importez mongoose dans votre fichier 
+const mongoose = require('mongoose');
+//package de validation pour prévalider les informations avant de les enregistrer
+const uniqueValidator = require('mongoose-unique-validator');
 
-//function enregistrement de nouveaux utilisateur (Middleware d'authentification)
+//importer notre schema et utilisation de la function de mongoose
+const userSchema = mongoose.Schema({
+    mail: {type : String, require: true , unique: true }, // unique true pour eviter de s'inscrire plusieur fois
+    password: { type: String, required: true }
+}); //require true champ requis
 
-//crypter le mdp , cree un new user avec hash +email et enregistrer user dans la bdd
-exports.signup = (req, res, next) => { //function pour crypter un mot de pass , on lui pass de mdp
-    bcrypt.hash(req.body.password, 10) // 10 tour pour verifier l'algoritme (methode asyncrone)
-    .then(hash => { // recuper le hash(mdp crypter)  de mdp 
-        //creation du new utilisateur
-        const user = new User({ //creation un nouvelle utilisateur (user) avec monggose
-        email: req.body.email, // email passez l'addresse passsez dans le corp de la requete
-        password: hash // enregistrer le mdp crypter (hash) pour ne pas stocker un mdp en blanc
-        });
-    user.save() //enregistre dans la BDD    
-    .then(() => res.status(201).json({message: 'utilisateur créé !'})) //creation de ressource
-    .catch(error => res.status(400).json({ error })); //impossible de se connecter au serveur
-})
-.catch(error => res.status(500).json({ error })); // 500 erreur serveur renvoi erreur dans objet
-};  
+//s'assurera que deux utilisateurs ne puissent partager la même adresse e-mail.
+userSchema.plugin(uniqueValidator);
 
-
-
-
-//function login pour connecter de nouveaux utilisateur existant
-
-exports.login = (req, res, next) => {
-
-};
-
-//----------------
+// exporté model de mongoose , transforme ce modèle en un modèle utilisable (nom du model,schema)
+module.exports = mongoose.model('User', userSchema); //schema de donnée
