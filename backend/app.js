2d2b5f62f14e85d8b847b45af100cdeb2e1e93db
P2-openclassroom <jonh_06@live.fr>
2022-03-15 12:38:58 +0100

    change com

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/user');  //enregistrer notre nouveau routeu
 // appelle de la methode express (une function) permet de crée une application expresse
 const app = express();
 
-// -----------conection a la base de donnée mongoose -----------
+// -----------conexion a la base de donnée mongoose -----------
 mongoose.connect(`${process.env.DB_LOGIN_ACCOUNT}`, // DB_LOGIN_ACCOUNT identifiant utilisateur login du .env
 {     
     useNewUrlParser: true,
@@ -20,23 +20,31 @@ mongoose.connect(`${process.env.DB_LOGIN_ACCOUNT}`, // DB_LOGIN_ACCOUNT identifi
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 //---------------------
 
-//--------Middleware-------- (permet a l'application d'accedera l'api)
+//--------Acces serveurs (CORS)
 
-app.use((req, res, next) => { //reponse par default et donc retourn bien notre application on recoit l'objet request response et next pour passez middleware suivant
+//reponse par default et donc retourn bien notre application on recoit l'objet request response et next pour passez middleware suivant
+app.use((req, res, next) => { 
     // '*' signifie all tous le monde  a acces au serveur origin
     res.setHeader('Access-Control-Allow-Origin', '*'); //header ajouter un header aux routes  setheader sur nos response 
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //autorise certaine header (en tete)sur l'objet requete
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //autorise certaine methode requete
     next();
 });
-//acces au corp de la requete (body) et met a disposition dans le corp (acces) sur objet req  (body)
+///---------acces au corp de la requete (body) et met a disposition dans le corp (acces) sur objet req  (body)
+
+
 app.use(express.json());// intercepte toute les requetes qui on content type json (format) 
-app.use('/api/sauces', sauceRoutes);// enregistrer notre routeur pour toutes les demandes effectuées vers /api/sauces
+
 // dire a expresse de servir ce dossier images
 // servir un dossier static avec cette methode , (ajouter .join) nom du dossier ou on se trouve et ajouter images
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/auth', userRoutes); //route atendu par le front end
 
+// multer gerer les fichier (image)---------
+app.use('/images', express.static(path.join(__dirname, 'images')));
+//----
+
+// debut des Routes
+app.use('/api/auth', userRoutes); //route atendu par le front end
+app.use('/api/sauces', sauceRoutes);// enregistrer notre routeur pour toutes les demandes effectuées vers /api/sauces
 //------------------
 
 // exporter cette application pour y avoir acces depuis les autre fichier (app.js) de notre projet notament le server node
