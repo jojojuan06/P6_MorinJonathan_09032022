bb50f60fe90215d05ce370836935bda3f55c8e74
P2-openclassroom <jonh_06@live.fr>
2022-03-17 16:28:22 +0100

    change rate limit

--- backend/middleware/rate-limit.js
+++ backend/middleware/rate-limit.js
@@ -6,19 +6,19 @@ const  rateLimit  =  require( 'express-rate-limit' );
 const limiter = {
     get: rateLimit({ //pour les route get
          windowMs: 1 * 60 * 1000, // 1 minute
-         max: 60, // Limit each IP to 60 request per `window`
+         max: 60, // Limitez chaque IP à 60 requêtes par `window` 
          standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
          legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     }),
     modify: rateLimit({ // (post,put)
          windowMs: 10 * 60 * 1000, // 10 minutes (min,seconde,ms(1s))
-         max: 20, // Limit each IP to 20 requests per `window` (here, per 10 minutes)
+         max: 20, // Limitez chaque IP à 20 requêtes par `window` (ici, par 10 minutes)
         standardHeaders: true,
         legacyHeaders: false,
     }),
     auth: rateLimit({
          windowMs: 10 * 60 * 1000, // 10 minutes
-         max: 5, // Limit each IP to 5 requests per `window` (here, per 10 minutes)
+         max: 10, // Limitez chaque IP à 5 requêtes par `window` (ici, par 10 minutes)
         standardHeaders: true,
         legacyHeaders: false,
     })
